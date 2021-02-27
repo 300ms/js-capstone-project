36b6b65c1ba7676efc85018fafcbcaaa802c7b1c
300ms <ilhansnmz44@gmail.com>
2021-02-28 00:31:12 +0300

    Add simple tests to see jest is working

---
+++ src/test/unit.test.js
@@ -0,0 +1,22 @@
+import Enemy from '../units/enemy';
+import PlayerCharacter from '../units/playerCharacter';
+
+jest.mock('../units/enemy');
+jest.mock('../units/playerCharacter');
+
+beforeEach(() => {
+  Enemy.mockClear();
+  PlayerCharacter.mockClear();
+});
+
+it('works', () => {
+  expect(PlayerCharacter).not.toHaveBeenCalled();
+  expect(Enemy).not.toHaveBeenCalled();
+
+  const warrior = new PlayerCharacter(this, 250, 50, 'player', 1, 'Warrior', 10, 5);
+  const mage = new PlayerCharacter(this, 250, 100, 'player', 4, 'Mage', 10, 5);
+  const dragon = new Enemy(this, 50, 50, 'enemy', 17, 'Dragon', 11, 3);
+
+  expect(PlayerCharacter).toHaveBeenCalledTimes(2);
+  expect(Enemy).toHaveBeenCalledTimes(1);
+});
